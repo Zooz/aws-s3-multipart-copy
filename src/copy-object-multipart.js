7f2bdd9ee3a0f53898885a6d902d0d8b1ca7fb03
mikkel-pearl <79226700+mikkel-pearl@users.noreply.github.com>
2021-04-26 10:57:47 +1200

    Added storage class param

--- src/copy-object-multipart.js
+++ src/copy-object-multipart.js
@@ -29,8 +29,12 @@ const init = function (aws_s3_object, initialized_logger) {
  * (note that copy_part_size_bytes, copied_object_permissions, expiration_period are optional and will be assigned with default values if not given)
  * @param {*} request_context optional parameter for logging purposes
  */
-const copyObjectMultipart = async function ({ source_bucket, object_key, destination_bucket, copied_object_name, object_size, copy_part_size_bytes, copied_object_permissions, expiration_period, server_side_encryption, content_type, content_disposition, content_encoding, content_language, metadata, cache_control }, request_context) {
-    const upload_id = await initiateMultipartCopy(destination_bucket, copied_object_name, copied_object_permissions, expiration_period, request_context, server_side_encryption, content_type, content_disposition,  content_encoding, content_language, metadata, cache_control);
+const copyObjectMultipart = async function ({ source_bucket, object_key, destination_bucket, copied_object_name, object_size, copy_part_size_bytes, copied_object_permissions, expiration_period, server_side_encryption, content_type, content_disposition, content_encoding, content_language, metadata, cache_control, storage_class }, request_context) {
+    if (object_size < COPY_PART_SIZE_MINIMUM_BYTES) {
+        throw new Error("Multipart copy only works with files at least 5MB in size. Use s3.copyObject instead")
+    }
+    
+    const upload_id = await initiateMultipartCopy(destination_bucket, copied_object_name, copied_object_permissions, expiration_period, request_context, server_side_encryption, content_type, content_disposition,  content_encoding, content_language, metadata, cache_control, storage_class);
     const partitionsRangeArray = calculatePartitionsRangeArray(object_size, copy_part_size_bytes);
     const copyPartFunctionsArray = [];
 
@@ -50,7 +54,7 @@ const copyObjectMultipart = async function ({ source_bucket, object_key, destina
         });
 };
 
-function initiateMultipartCopy(destination_bucket, copied_object_name, copied_object_permissions, expiration_period, request_context, server_side_encryption, content_type, content_disposition, content_encoding, content_language, metadata, cache_control) {
+function initiateMultipartCopy(destination_bucket, copied_object_name, copied_object_permissions, expiration_period, request_context, server_side_encryption, content_type, content_disposition, content_encoding, content_language, metadata, cache_control, storage_class) {
     const params = {
         Bucket: destination_bucket,
         Key: copied_object_name,
@@ -64,6 +68,7 @@ function initiateMultipartCopy(destination_bucket, copied_object_name, copied_ob
     metadata ? params.Metadata = metadata : null;
     cache_control ? params.CacheControl = cache_control : null;
     server_side_encryption ? params.ServerSideEncryption = server_side_encryption : null;
+    storage_class ? params.StorageClass = storage_class : null;
 
     return s3.createMultipartUpload(params).promise()
         .then((result) => {
